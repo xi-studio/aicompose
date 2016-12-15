75dc7b450600d0a8c416be4f3cb593dc39a9361a
xi-studio <110littletree@sina.com>
2016-12-15 16:19:48 +0800

    somet thing to test

--- aicompose/ViewController.m
+++ aicompose/ViewController.m
@@ -7,6 +7,9 @@
 //
 
 #import "ViewController.h"
+#import <MIKMIDI/MIKMIDI.h>
+
+
 
 @interface ViewController ()
 
@@ -17,6 +20,15 @@
 - (void)viewDidLoad {
     [super viewDidLoad];
     // Do any additional setup after loading the view, typically from a nib.
+   
+    NSDate *date = [NSDate date];
+    MIKMIDINoteOnCommand *noteOn = [MIKMIDINoteOnCommand noteOnCommandWithNote:60 velocity:127 channel:0 timestamp:date];
+    MIKMIDINoteOffCommand *noteOff = [MIKMIDINoteOffCommand noteOffCommandWithNote:60 velocity:0 channel:0 timestamp:[date dateByAddingTimeInterval:0.5]];
+    
+    MIKMIDIDeviceManager *dm = [MIKMIDIDeviceManager sharedDeviceManager];
+    //[dm sendCommands:@[noteOn, noteOff] toEndpoint:destinationEndpoint error:&error];
+   
+   
 }
 
 
